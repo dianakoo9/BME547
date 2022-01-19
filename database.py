c40f285f1d48227ef169d1045a74016360833c8f
Diana <ddk15@duke.edu>
2022-01-19 16:11:06 -0500

    Branch to play with module importing

---
+++ database.py
@@ -0,0 +1,9 @@
+print("This is the database module and python calls it {}".format(__name__))
+
+from blood_calculator import check_HDL, check_LDL
+
+HDL_value = 55
+
+classification = blood_calculator.check_HDL(HDL_value)
+print("55 is {}".format(classification))
+x = check_LDL(200)
